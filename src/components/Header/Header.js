0a8f2fe877223e39594307f38a36eed441caad6d
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-22 13:48:10 -0500

    created header component, test passed, deleted non-needed files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.css";
+
+function Header() {
+	return <div></div>;
+}
+
+export default Header;
--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Header";
+import { render } from '@testing-library/react'
 
 test("loads and displays header", async () => {
 	render(<Header />);
