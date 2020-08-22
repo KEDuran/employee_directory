615b01342b8d37a415df74c65e24a1095ffd5df6
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-22 15:02:31 -0500

    created table component, test passed, added to App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Table from "./components/Table/Table";
 
 function App() {
-	return (<Header />);
+	return (
+		<div>
+			<Header />
+			<Table />
+		</div>
+	);
 }
 
 export default App;
