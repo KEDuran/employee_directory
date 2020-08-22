11f12440085b2ab45750f0d6163a22b0883fbddc
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-22 13:35:09 -0500

    moving test into the correct test.js file

--- src/components/Header/Header.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Header from "./Header";
-
-test("loads and displays header", async () => {
-	render(<Header />);
-});
---
+++ src/components/Header/Header.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Header from "./Header";
+
+test("loads and displays header", async () => {
+	render(<Header />);
+});
