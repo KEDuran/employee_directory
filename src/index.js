7b885a83d69cb666404f1f5481da22fc6a3ae67f
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-23 13:44:25 -0500

    added console.warn to remove unnecessary error resulting from third-party module

--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
+/*Disables UNNECESSARY warnings associated with react-scripts, specifically resultings from 
+using mdreact components. Referenced this article for source code  -
+https://stackoverflow.com/questions/41111338/how-to-clear-disable-minor-and-warnings-console-log-error */
+console.warn = console.error = () => {};
+
 ReactDOM.render(<App />, document.getElementById("root"));
