36b7a74b0be6b2b0f207474ef34171fa3ebfc5d6
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-22 20:58:18 -0500

    modified the axios test to fail properly

--- src/components/TableComp/TableComp.test.js
+++ src/components/TableComp/TableComp.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import TableComp from "./TableComp";
 import { render } from "@testing-library/react";
 import axios from "axios";
-import jest from "jest";
+// import jest from "jest";
 
 jest.mock("axios");
 
@@ -18,6 +18,7 @@ describe("TableComp", () => {
 	});
 	// test to make sure data is calling from API
 	test("fetches data from an API", async () => {
+		// creates a mock employee JSON object
 		const employees = {
 			results: [
 				{
@@ -29,6 +30,13 @@ describe("TableComp", () => {
 				},
 			],
 		};
+		// binds the axios calls to the employees JSON object
 		axios.get.mockImplementationOnce(() => Promise.resolve(employees));
+		// table component will make API call when rendered
+		render(<TableComp />);
+		// checks for AXIOS to call from the API URL
+		expect(axios.get).toHaveBeenCalledWith(
+			`https://randomuser.me/api/?results=200&nat=us`
+		);
 	});
 });
