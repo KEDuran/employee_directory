e495c80e97f4b02d72c51e013d0113fffd774b94
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-23 17:56:36 -0500

    added a test to test for rendering a Row component

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./Header";
-import { render } from '@testing-library/react'
+import { render } from "@testing-library/react";
+import { Row } from "react-bootstrap";
 
-
-// test to make sure the header renders
+// test to make sure the header component renders
 test("loads and displays header", async () => {
 	render(<Header />);
 });
+// test to see if row component renders
+test("renders a row component", async () => {
+	render(<Row></Row>)
+})
