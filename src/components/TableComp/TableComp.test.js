ccc8148668356b8bce3116a3ed698feef4c38938
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-08-22 20:35:23 -0500

    recreated new package.json - still creating test

--- src/components/TableComp/TableComp.test.js
+++ src/components/TableComp/TableComp.test.js
@@ -1,6 +1,10 @@
 import React from "react";
 import TableComp from "./TableComp";
 import { render } from "@testing-library/react";
+import axios from "axios";
+import jest from "jest";
+
+jest.mock("axios");
 
 describe("TableComp", () => {
 	// test to make sure the table renders
@@ -11,5 +15,20 @@ describe("TableComp", () => {
 	// test to make sure the table renders
 	test("renders table body", async () => {
 		render("<tbody></tbody>");
+	});
+	// test to make sure data is calling from API
+	test("fetches data from an API", async () => {
+		const employees = {
+			results: [
+				{
+					picture: { thumbnail: "a" },
+					name: { first: "k", last: "j" },
+					phone: "123",
+					email: "test@test.com",
+					dob: { date: "1972-01-15" },
+				},
+			],
+		};
+		axios.get.mockImplementationOnce(() => Promise.resolve(employees));
 	});
 });
